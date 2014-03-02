2dd2f7d0457711f1c3bd138c4630c8e851d984c2
Ryan Spraetz <spraetz@gmail.com>
2014-03-02 12:20:13 -0800

    First junit test checkin

---
+++ src/test/java/com/gmail/spraetz/FirstTest.java
@@ -0,0 +1,20 @@
+package com.gmail.spraetz;
+
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+/**
+ * Created by spraetz on 3/2/14.
+ */
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(PlayerJoinEvent.class)
+public class FirstTest {
+    @Test
+    public void testPlayerJoin(){
+        
+    }
+}
