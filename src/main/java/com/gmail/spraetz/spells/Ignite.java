018bda78e3d731bb6ac8ed311436261dc45327a1
Ryan Spraetz <spraetz@gmail.com>
2014-03-20 20:08:39 -0700

    Add ignite spell

---
+++ src/main/java/com/gmail/spraetz/spells/Ignite.java
@@ -0,0 +1,30 @@
+package com.gmail.spraetz.spells;
+
+import com.gmail.spraetz.plugin.MineCraftSpells;
+import org.bukkit.entity.Damageable;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+
+/**
+ * Created by spraetz on 3/20/14.
+ */
+public class Ignite extends TouchSpell {
+
+    public Ignite(EntityDamageByEntityEvent event, MineCraftSpells plugin) {
+        super(event, plugin);
+    }
+
+    @Override
+    public Boolean spellEffects(EntityDamageByEntityEvent event, String spellName) {
+
+        if(event.getEntity() instanceof Damageable){
+
+            //Set the target on fire.
+            event.getEntity().setFireTicks(getSetting(spellName, "fire_ticks", Integer.class));
+
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
